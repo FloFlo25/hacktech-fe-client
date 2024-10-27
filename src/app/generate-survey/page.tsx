37434f3dbd5236202f08c4d887126ed3eee829fa
Raul <floruta.raul25@gmail.com>
2024-10-27 12:58:34 +0200

    update

--- src/app/generate-survey/page.tsx
+++ src/app/generate-survey/page.tsx
@@ -1,16 +1,20 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { generateSurvey } from "~/api/form";
+import SurveyCard from "~/components/SurveyCard";
+import { surveyJson } from "../../lib/utils";
 
 const GenerateSurvey = () => {
+	const [surveyJson, setSurveyJson] = useState(null);
 	useEffect(() => {
 		try {
-			if (localStorage.getItem("extractedKeywords"))
-				console.log(
-					generateSurvey({
-						keywords: JSON.parse(localStorage.getItem("extractedKeywords")),
-					}),
-				);
+			if (localStorage.getItem("extractedKeywords")) {
+				const response = generateSurvey({
+					keywords: localStorage.getItem("extractedKeywords"),
+				});
+				console.log(response);
+				// setSurveyJson(response);
+			}
 		} catch (error) {
 			console.error("Error generating survey:", error);
 		}
@@ -19,7 +23,9 @@ const GenerateSurvey = () => {
 	return (
 		<div className="w-full p-8">
 			<div className="w-fit rounded-[24px] bg-primary-secondary p-8">
-				<div className="flex min-h-[800px] min-w-[917px] flex-col items-center gap-8 rounded-[12px] bg-white p-8"></div>
+				<div className="flex min-h-[800px] min-w-[917px] flex-col items-center gap-8 rounded-[12px] bg-white p-8">
+					{/* <SurveyCard surveyData={surveyJson.survey[0]} /> */}
+				</div>
 			</div>
 		</div>
 	);
